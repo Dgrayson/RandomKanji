cc10bf3720ae0c7ce0350e155700a820f9b53bd9
Dgrayson <dgrayson100@gmail.com>
2021-02-03 22:19:26 -0500

    App now pulls from the Kanji api

--- src/Components/Kanji.js
+++ src/Components/Kanji.js
@@ -4,33 +4,30 @@ import '../App.css';
 
 function Kanji() {
 
+    let allKanji = []
     const [data, setData] = useState({ });
     const [isBusy, setBusy] = useState(true); 
 
     useEffect(async () => {
-        /*const result = await axios(
 
-        ); */
+        const response = await Promise.all([axios.get("http://kanjiapi.dev/v1/kanji/all")]); 
 
-        setData({
-            "kanji": "蛍",
-            "grade": 8,
-            "stroke_count": 11,
-            "meanings": [
-                "lightning-bug",
-                "firefly"
-            ],
-            "kun_readings": [
-                "ほたる"
-            ],
-            "on_readings": [
-                "ケイ"
-            ],
-            "name_readings": [],
-            "jlpt": 1,
-            "unicode": "86cd",
-            "heisig_en": "lightning-bug"
-        });
+        console.log(response); 
+        console.log("Data is: " + response[0].data); 
+
+        console.log(response.length); 
+
+        let num = Math.floor(Math.random() * Math.floor(response[0].data.length)); 
+        
+        console.log(num); 
+
+        const kanji = await axios.get("http://kanjiapi.dev/v1/kanji/" + response[0].data[num]); 
+
+        console.log(kanji.data); 
+
+        setData(
+            kanji.data
+        );
 
         setBusy(false); 
     }, []);
@@ -45,11 +42,9 @@ function Kanji() {
                     <h2>Definitions</h2>
 
                     <div className="readings">
-                        <ul>
-                            {data.meanings.map((item) =>
-                                <li className="meanings-list">{item}</li>
-                            )}
-                        </ul>
+                        {data.meanings.map((item) =>
+                            <div className="meanings-list">{item}</div>
+                        )}
                     </div>
                 </div>
                 <div className="KanjiData">
@@ -61,21 +56,18 @@ function Kanji() {
                         <h2>Kunyomi Readings:</h2>
 
                         <ul>
-                            {
-                                data.kun_readings.map((item) =>
-                                    <li className="kun-readings">{item}</li>
-                                )
-                            }
+                            {data.kun_readings != null || data.kun_readings == []
+                                ? data.kun_readings.map((item) => <li className="kun-readings">{item}</li>) 
+                                : "No data found"}
                         </ul>
                     </div>
 
                     <div>
                         <h2>On-yomi Readings: </h2>
                         <ul>
-                            {
-                                data.on_readings.map((item) =>
-                                    <li>{item}</li>
-                                )
+                            {data.on_readings != null || data.on_readings == []
+                                ? data.on_readings.map((item) =><li>{item}</li>)
+                                : "No data found"
                             }
                         </ul>
                     </div>
