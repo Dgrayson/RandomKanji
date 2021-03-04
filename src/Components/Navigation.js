32d1a1019f36481b54ef10b65ee53354c56d493f
Darayavaus Grayson <dgrayson100@gmail.com>
2021-03-04 01:58:34 -0500

    Added navigation and filters

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,8 +4,19 @@ import '../App.css';
 import { render } from '@testing-library/react';
 
 function Navigation(){
+
+    const [mode, SetMode] = useState("All"); 
+    
     return(
         <div>
+            <select>
+                <option>All</option>
+                <option>Grade 1</option>
+                <option>Grade 2</option>
+                <option>Grade 3</option>
+                <option>Grade 4</option>
+                <option>Grade 5</option> 
+            </select>
             <button>Give me another</button>
         </div>
     );
