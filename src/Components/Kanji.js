c6a89fcb44501ac645516f8d8c2947b946669771
Dgrayson <dgrayson100@gmail.com>
2021-08-20 23:08:43 -0400

    Splitting main component into smaller components

--- src/Components/Kanji.js
+++ src/Components/Kanji.js
@@ -2,6 +2,52 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css'; 
 
+const Readings = ({data}) =>{
+
+    return (
+        <div class="readings">
+            <div id="kun-readings">
+                <KunyomiReading  data={data} />
+            </div>
+            
+            <div id="on-readings">
+                <OnyomiReading data={data} />
+            </div>
+        </div>
+    )
+}
+
+const KunyomiReading = ({data}) =>{
+    return (
+        <div id="kun-readings">
+            <h2>Kunyomi Readings:</h2>
+
+            <ul class="kun-readings-list">
+                {
+                    data.kun_readings != null || data.kun_readings == []
+                        ? data.kun_readings.map((item) => <li className="kun-entry">{item}</li>) 
+                        : <li>No data found</li>
+                }
+            </ul>
+        </div>
+    )
+}
+
+const OnyomiReading = ({data}) =>{
+    return (
+        <div >
+            <h2>On-yomi Readings: </h2>
+            <ul class="on-readings-list">
+                {
+                    data.on_readings != null || data.on_readings == []
+                        ? data.on_readings.map((item) =><li>{item}</li>)
+                        : <li>No data found</li>
+                }
+            </ul>
+        </div>
+    )
+}
+
 function Kanji() {
 
     let allKanji = []
@@ -58,30 +104,9 @@ function Kanji() {
                     </div>
                 </div>
 
+                <Readings data={data}/>         
 
 
-                    <div id="kun-readings">
-                        <h2>Kunyomi Readings:</h2>
-
-                        <ul class="kun-readings-list">
-                            {
-                                data.kun_readings != null || data.kun_readings == []
-                                    ? data.kun_readings.map((item) => <li className="kun-entry">{item}</li>) 
-                                    : "No data found"
-                            }
-                        </ul>
-                    </div>
-
-                    <div id="on-readings">
-                        <h2>On-yomi Readings: </h2>
-                        <ul class="on-readings-list">
-                            {
-                                data.on_readings != null || data.on_readings == []
-                                    ? data.on_readings.map((item) =><li>{item}</li>)
-                                    : "No data found"
-                            }
-                        </ul>
-                    </div>
 
                 <div id="nav-button">
                     <select id="filter" onChange={changeMode}>
